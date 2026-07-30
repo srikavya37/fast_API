05cb4508c5ca210a159a199b0f8c208c8c2de8aa
Vetcha Srikavya <155564499+srikavya37@users.noreply.github.com>
2026-07-30 14:41:34 +0530

    Update DATABASE_URL for database connection

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/mobiles_db"
+#DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/mobiles_db"
+DATABASE_URL="mysql+pymysql://mysql-1502a305-vetchasrikavya77-984c.k.aivencloud.com:24214/defaultdb"
 
 engine = create_engine(DATABASE_URL)
 
