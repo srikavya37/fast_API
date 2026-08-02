1e1068aa92501dd24e842581ca0a33d5f6fb4eb4
sriKavya37 <srikavyavetcha37@gmail.com>
2026-08-02 23:22:14 +0530

    updating database url

--- database.py
+++ database.py
@@ -7,7 +7,6 @@ load_dotenv()
 
 #DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/mobiles_db"
 DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_haq_unkwgk-4nB_Wa5U@mysql-1502a305-vetchasrikavya77-984c.k.aivencloud.com:24214/defaultdb"
-DATABASE_URL=os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
