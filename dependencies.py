f49323d9a686c779808dfaf69a65eb1740694ac1
sriKavya37 <srikavyavetcha37@gmail.com>
2026-08-03 22:08:11 +0530

    adding security.py file

--- dependencies.py
+++ dependencies.py
@@ -7,12 +7,16 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
+    print("TOKEN RECEIVED:", token)
+
     try:
         payload = jwt.decode(
             token,
             SECRET_KEY,
             algorithms=[ALGORITHM]
         )
+
+        print("PAYLOAD:", payload)
 
         username = payload.get("sub")
         role = payload.get("role")
@@ -28,7 +32,8 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
             "role": role
         }
 
-    except JWTError:
+    except Exception as e:
+        print("JWT ERROR:", str(e))
         raise HTTPException(
             status_code=401,
             detail="Invalid Token"
