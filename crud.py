08b8ca2521caf51c9144278580449e060d5bf66e
sriKavya37 <srikavyavetcha37@gmail.com>
2026-08-03 22:48:05 +0530

    update the code in crud.py to include error handling for database operations.

--- crud.py
+++ crud.py
@@ -1,16 +1,35 @@
 #crud
-from sqlalchemy.orm import Session  # type: ignore
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
 import models
 import schemas
 
+from fastapi import HTTPException
 
 def create_mobile(db: Session, mobile: schemas.MobileCreate):
+
+    existing_mobile = db.query(models.Mobile).filter(
+        models.Mobile.brand == mobile.brand,
+        models.Mobile.model == mobile.model,
+        models.Mobile.ram == mobile.ram,
+        models.Mobile.storage == mobile.storage,
+        models.Mobile.color == mobile.color
+    ).first()
+
+    if existing_mobile:
+        raise HTTPException(
+            status_code=400,
+            detail="Mobile already exists"
+        )
+
     db_mobile = models.Mobile(**mobile.model_dump())
+
     db.add(db_mobile)
     db.commit()
     db.refresh(db_mobile)
-    return db_mobile
 
+    return db_mobile
 
 def get_mobiles(db: Session):
     return db.query(models.Mobile).all()
